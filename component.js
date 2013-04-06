351702979dd3e2aab769e42759b9211dd1eac7c3
pickypg <pickypg@gmail.com>
2013-04-06 03:33:15 -0300

    component

---
+++ component.js
@@ -0,0 +1,8 @@
+{
+  "name": "bootstrap-formvalidation",
+  "version": "0.1",
+  "main": ["./docs/assets/js/bootstrap-formvalidation.js"],
+  "dependencies": {
+    "jquery": "~1.8.0" // or higher
+  }
+}
